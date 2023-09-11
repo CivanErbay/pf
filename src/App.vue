b3edd443237afca45891b83d26eac6bedecb5722
Civan <Erbay.Civan.Extern@swm.de>
2023-09-11 18:18:29 +0200

    Setup

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import SvgScroll from "./components/SvgScroll.vue";
+</script>
+
+<template>
+  <!--   <header></header> -->
+
+  <main>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/cvline.svg" /> -->
+    <SvgScroll></SvgScroll>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  /*   header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  } */
+
+  .logo {
+    height: 200vh;
+  }
+
+  /*   header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  } */
+}
+</style>
---
+++ src/components/SvgScroll.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="svg-container">
+    <svg width="404" height="897" viewBox="0 0 200 300" fill="none">
+      <path
+        ref="path"
+        id="svgPath"
+        d="M0.5 247L44 228V216L28 197.5L20.5 181L11 150.5V133.5L15.5 122.5L7 106.5L3 73.5L20.5 36L44 7L73 1H79L101 7L133 39.5L159.5 78L156 115L153 126.5L156 133.5V157L147 181H141.5H136.5L133 194.5L128 205.5L136.5 222L182 247M20.5 115L24.5 87.5L33.5 66.5L60 62.5L76.5 67.5L79 62.5L93 66.5L104.5 57.5L120.5 67.5L133 101L136.5 133.5L138.5 148L145 136M79 142.5C79 142.5 91.5 151 89 157C86.5 163 76.5 166 76.5 166L64 163L53.5 157M51.5 230.5L64 242H89L115.5 216M20.5 124C24.5 116.5 28 115 33.5 112C39 109 44 112 50 115C56 118 60 122.5 60 126.5C60 130.5 79 122.5 79 122.5C79 122.5 84.5 110 89 106.5C93.5 103 103 104.5 111 106.5C119 108.5 128 115 128 122.5C128 130 122 139.5 115.5 142.5C109 145.5 93 142.5 93 142.5L82.5 130C82.5 130 66.5417 127.477 60 133.5C55.8244 137.345 63 142.5 53.5 146.5C44 150.5 33.5 148 33.5 148C33.5 148 25.5 146.5 20.5 140C15.5 133.5 20.5 124 20.5 124ZM44 176.5L64 194.5C64 194.5 82.5 200 89 194.5C95.5 189 104.5 176.5 104.5 176.5C104.5 176.5 86 181 73 181C60 181 44 176.5 44 176.5ZM28 106.5L40.5 101H57L50 94.5H33.5L28 106.5ZM82.5 97.5L101 91L115.5 94.5L125.5 105.5L104.5 97.5H82.5Z"
+        stroke="black"
+        stroke-dasharray="0"
+        stroke-dashoffset="0"
+      />
+    </svg>
+    <img alt="civan erbay" class="me" src="../assets/pixel_me.png" />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {},
+  methods: {
+    fillingSVGPaths() {
+      let scrollPercentage =
+        (document.documentElement.scrollTop + document.body.scrollTop) /
+        (document.documentElement.scrollHeight -
+          document.documentElement.clientHeight);
+
+      console.log(scrollPercentage);
+
+      let path = document.getElementById("svgPath");
+
+      let pathLength = path.getTotalLength();
+
+      path.style.strokeDasharray = pathLength;
+      path.style.strokeDashoffset = pathLength;
+
+      let drawLength = pathLength * scrollPercentage;
+      console.log(drawLength);
+      console.log(pathLength);
+      path.style.strokeDashoffset = pathLength - drawLength;
+    },
+  },
+  mounted() {
+    window.addEventListener("scroll", this.fillingSVGPaths);
+  },
+  beforeDestroy() {
+    window.removeEventListener("scroll", this.fillingSVGPaths);
+  },
+};
+</script>
+
+<style scoped>
+.svg-container {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 200px;
+  position: relative;
+}
+
+.me {
+    position: absolute;
+    height: 500px;
+}
+</style>
